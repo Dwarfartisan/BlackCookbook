87eacc0ba82d75c7319b23bbbacd55d637c09dd1
gislu <ahlxt@qq.com>
2015-10-08 11:25:23 +0800

    originol diff

---
+++ python/JsonDiff/treedt.py
@@ -0,0 +1,131 @@
+import collections
+
+try:
+    import numpy as np
+    zeros = np.zeros
+except ImportError:
+    def py_zeros(dim, pytype):
+        assert len(dim) == 2
+        return [[pytype() for y in xrange(dim[1])]
+                for x in xrange(dim[0])]
+    zeros = py_zeros
+
+
+from treestruct import Node
+
+
+class AnnotatedTree(object):
+
+    def __init__(self, root, get_children):
+        self.get_children = get_children
+
+        self.root = root
+        self.nodes = list()  
+        self.ids = list()    
+        self.lmds = list()  
+        self.keyroots = None
+        stack = list()
+        pstack = list()
+        stack.append((root, collections.deque()))
+        j = 0
+        while len(stack) > 0:
+            n, anc = stack.pop()
+            nid = j
+            for c in self.get_children(n):
+                a = collections.deque(anc)
+                a.appendleft(nid)
+                stack.append((c, a))
+            pstack.append(((n, nid), anc))
+            j += 1
+        lmds = dict()
+        keyroots = dict()
+        i = 0
+        while len(pstack) > 0:
+            (n, nid), anc = pstack.pop()
+            #print list(anc)
+            self.nodes.append(n)
+            self.ids.append(nid)
+            #print n.label, [a.label for a in anc]
+            if not self.get_children(n):
+                lmd = i
+                for a in anc:
+                    if a not in lmds: lmds[a] = i
+                    else: break
+            else:
+                try: lmd = lmds[nid]
+                except:
+                    import pdb
+                    pdb.set_trace()
+            self.lmds.append(lmd)
+            keyroots[lmd] = i
+            i += 1
+        self.keyroots = sorted(keyroots.values())
+
+def strdist(a, b):
+    if a == b:
+        return 0
+    else:
+        return 1
+
+def jstree_distance(A, B, get_children=Node.get_children,
+        get_label=Node.get_label, label_dist=strdist):
+
+    return distance(
+        A, B, get_children,
+        insert_cost=lambda node: label_dist('', get_label(node)),
+        remove_cost=lambda node: label_dist(get_label(node), ''),
+        update_cost=lambda a, b: label_dist(get_label(a), get_label(b)),
+    )
+
+
+def distance(A, B, get_children, insert_cost, remove_cost, update_cost):
+
+    A, B = AnnotatedTree(A, get_children), AnnotatedTree(B, get_children)
+    treedists = zeros((len(A.nodes), len(B.nodes)), int)
+
+    def treedist(i, j):
+        Al = A.lmds
+        Bl = B.lmds
+        An = A.nodes
+        Bn = B.nodes
+
+        m = i - Al[i] + 2
+        n = j - Bl[j] + 2
+        fd = zeros((m,n), int)
+
+        ioff = Al[i] - 1
+        joff = Bl[j] - 1
+
+        for x in xrange(1, m): 
+            fd[x][0] = fd[x-1][0] + remove_cost(An[x+ioff])
+        for y in xrange(1, n): 
+            fd[0][y] = fd[0][y-1] + insert_cost(Bn[y+joff])
+
+        for x in xrange(1, m): ## the plus one is for the xrange impl
+            for y in xrange(1, n):
+                # only need to check if x is an ancestor of i
+                # and y is an ancestor of j
+                if Al[i] == Al[x+ioff] and Bl[j] == Bl[y+joff]:
+                    
+                    fd[x][y] = min(
+                        fd[x-1][y] + remove_cost(An[x+ioff]),
+                        fd[x][y-1] + insert_cost(Bn[y+joff]),
+                        fd[x-1][y-1] + update_cost(An[x+ioff], Bn[y+joff]),
+                    )
+                    treedists[x+ioff][y+joff] = fd[x][y]
+                else:
+                    
+                    p = Al[x+ioff]-1-ioff
+                    q = Bl[y+joff]-1-joff
+                    #print (p, q), (len(fd), len(fd[0]))
+                    fd[x][y] = min(
+                        fd[x-1][y] + remove_cost(An[x+ioff]),
+                        fd[x][y-1] + insert_cost(Bn[y+joff]),
+                        fd[p][q] + treedists[x+ioff][y+joff]
+                    )
+
+    for i in A.keyroots:
+        for j in B.keyroots:
+            treedist(i,j)
+
+    return treedists[-1][-1]
